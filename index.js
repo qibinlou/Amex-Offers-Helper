af4df1d447cd29400c5fe66afa1c15e761ca9acd
bitbrian <bitbrian@bitbrian.com>
2019-03-19 14:07:38 +0900

    Update to support latest amex interface

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function main() {
-  const WAIT_TIME_MS = 2000;
-  const offerButtons = document.getElementsByClassName('selectbtn');
-  for (let i = 0; i < offerButtons.length; ++i) {
-    setTimeout(() => {
-      try {
-        console.log('Clicking ' + offerButtons[i].id);
-        offerButtons[i].click();
-      } catch (e) {
-        console.error(e);
-      }
-    }, i * WAIT_TIME_MS)
-  }
+	const WAIT_TIME_MS = 5000;
+	const offerButtons = document.getElementsByClassName('btn');
+	for (let i = 0; i < offerButtons.length; ++i) {
+		if (offerButtons[i].title == "Add to Card") {
+			setTimeout(() => {
+				try {
+					console.log('Clicking ' + offerButtons[i].id);
+					offerButtons[i].click();
+				} catch (e) {
+					console.error(e);
+				}
+			}, i * WAIT_TIME_MS)
+		}
+	}
 };
 
 main();
