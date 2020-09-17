71d9f4f1a46bbcc9753fa495fb1dce3a0bb5c3fe
Leo Lou <qibinlou@hotmail.com>
2020-09-17 08:58:20 -0400

    Update query selector to only select clickable offer buttons.
    Decrease wait time from 5s to 2s after testing.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 function main() {
-	const WAIT_TIME_MS = 5000;
-	const offerButtons = document.getElementsByClassName('btn');
+	const WAIT_TIME_MS = 2000;
+	const offerButtons = document.querySelectorAll('button.btn.offer-cta');
 	for (let i = 0; i < offerButtons.length; ++i) {
-		if (offerButtons[i].title == "Add to Card") {
-			setTimeout(() => {
-				try {
-					console.log('Clicking ' + offerButtons[i].id);
-					offerButtons[i].click();
-				} catch (e) {
-					console.error(e);
-				}
-			}, i * WAIT_TIME_MS)
-		}
+		setTimeout(() => {
+			try {
+				console.log('Clicking ' + offerButtons[i].id);
+				offerButtons[i].click();
+			} catch (e) {
+				console.error(e);
+			}
+		}, i * WAIT_TIME_MS)
 	}
 };
 
