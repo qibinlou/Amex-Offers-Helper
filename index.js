6cae920144b3d8fd7d86543fb94a079a54c38663
Ryan Sheatsley <ryan@sheatsley.me>
2021-11-27 14:27:29 -0500

    Update button class

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function main() {
 	const WAIT_TIME_MS = 2000;
-	const offerButtons = document.querySelectorAll('button.btn.offer-cta');
+	const offerButtons = document.querySelectorAll('button.btn.btn-sm.btn-fluid.offer-cta.btn-secondary');
 	for (let i = 0; i < offerButtons.length; ++i) {
 		setTimeout(() => {
 			try {
